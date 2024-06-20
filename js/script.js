2b5c1ca5bc816eacde17b8d9e8383137fd33bb4c
mohammadjodaki <jodakianm@gmail.com>
2024-06-20 21:30:13 +0330

    edit icon

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 let prevh3 = null;
-document.querySelectorAll('ul > li > h3').forEach((acch3) => {
+document.querySelectorAll('ul > li > h3').forEach ((acch3) => {
     acch3.addEventListener('click', () => {
         if (prevh3 && prevh3 !== acch3) {
             prevh3.nextElementSibling.classList.add('hidden');
-        }
+                }
         acch3.nextElementSibling.classList.toggle('hidden');
+        
         prevh3 = acch3;
-    });
+    })
 });
